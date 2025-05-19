8bdb173d78364de5f4a9e495674a3adfef357135
Charles Stoeter <charlesstoeter@gmail.com>
2025-05-18 23:59:42 -0400

    adding phone parts to lab1 hello world

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -12,14 +12,33 @@ int main() {
 		std::cout << "Failed to initialize Allegro\n";
 		return -1;
 	}
+
+	//keyboard
+	if (!al_install_keyboard()) {
+		std::cout << "Failed to install keyboard\n";
+		return -1;
+	}
+
 	// Initialize the font addon
 	if (!al_init_font_addon()) {
 		std::cout << "Failed to initialize font addon\n";
 		return -1;
 	}
 
+	// Initialize the ttf addon
+	if (!al_init_ttf_addon()) {
+		std::cout << "Failed to initialize ttf addon\n";
+		return -1;
+	}
+
+	//initialize the primitves addon
+	al_init_primitives_addon();
+
 	// Initialize the display
 	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
 
 	if (!display) {
 		std::cout << "Failed to create display\n";
@@ -29,13 +48,17 @@ int main() {
 	// Initialize starting screen/clear screen
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 
-	al_draw_filled_rectangle(200, 300, 400, 500, al_map_rgb(139, 69, 19)); // brown
+	al_draw_filled_rectangle(300, 100, 500, 500, al_map_rgb(50, 50, 50)); // white
+
+	al_draw_filled_rectangle(320, 140, 480, 440, al_map_rgb(200, 200, 200)); // light gray
+
+	al_draw_filled_circle(400, 470, 10, al_map_rgb(255, 255, 255)); // white
 
 	//flips display to the front
 	al_flip_display();
 
 
-	al_rest(5.0); // Wait for 5 seconds
+	al_rest(3.0); // Wait for 3 seconds
 
 	// clean up
 	al_destroy_display(display);
